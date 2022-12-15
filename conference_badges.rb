f8bab90a773cba1531ceadbb5d0400419611745f
Taylor <bKuzzcs@gmail.com>
2022-12-14 19:27:23 -0700

    commit tree 39c6abaf599b05ed82c4f65651db3aa4816bc8e2

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
 # Write your code here.
+
+def badge_maker name
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator names
+    names.map do |names|
+        "Hello, my name is #{names}."
+    end
+end
+
+def assign_rooms names
+    names.map.with_index(1) do |names, room|
+        "Hello, #{names}! You'll be assigned to room #{room}!"
+    end
+end
+
+def printer(names)
+    batch_badge_creator(names).each do |badge|
+      puts badge
+    end
+  
+    assign_rooms(names).each do |assignment|
+      puts assignment
+    end
+  end
